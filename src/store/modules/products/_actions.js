58b5ebd92f0b5d4cd950ea1de78e55063c18f200
Serhii Matrunchyk <serhii@digitalidea.studio>
2017-10-18 00:51:21 +0300

    Added force parsing as JSON for Windows

--- src/store/modules/products/_actions.js
+++ src/store/modules/products/_actions.js
@@ -4,6 +4,7 @@ export default {
     setProducts({ commit }, productUrl) {
         return axios.get(productUrl)
             .then(({ data }) => {
+                data = typeof data === 'string' ? JSON.parse(data) : data; // eslint-disable-line no-param-reassign
                 commit('setProducts', data);
             });
     },
--- src/store/modules/users/_actions.js
+++ src/store/modules/users/_actions.js
@@ -4,6 +4,7 @@ export default {
     setUsers({ commit }, remoteUrl) {
         return axios.get(remoteUrl)
             .then(({ data }) => {
+                data = typeof data === 'string' ? JSON.parse(data) : data; // eslint-disable-line no-param-reassign
                 commit('setUsers', data);
             });
     },
