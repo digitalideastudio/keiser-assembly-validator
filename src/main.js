90881612142022df8eb8c314e90f7e2cbfa65f04
Serhii Matrunchyk <serhii@digitalidea.studio>
2019-02-15 11:01:08 +0200

    Add apollo, pwa packages

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import Element from 'element-ui';
 import App from './App';
 import router from './router';
 import store from './store';
+import './registerServiceWorker';
+import { createProvider } from './vue-apollo';
 
 require('sweetalert');
 
@@ -17,5 +19,6 @@ Vue.prototype.$eventHub = new Vue();
 new Vue({
   router,
   store,
-  render: h => h(App)
+  apolloProvider: createProvider(),
+  render: h => h(App),
 }).$mount('#app');
